d98ce782976fe69c70152eef8f380736469d579f
Michael Callahan <CallahanMichaelE@gmail.com>
2020-02-28 10:32:49 -0700

    now have routing set - search bar created with reactstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Route } from 'react-router-dom';
 
 import WelcomePage from './components/WelcomePage'
 import SearchForm from './components/SearchForm'
-import CharacterCard from './components/CharacterCard'
 import CharacterList from './components/CharacterList'
 import Header from "./components/Header.js";
 
@@ -11,21 +10,15 @@ import Header from "./components/Header.js";
 export default function App() {
   return (
     <div>
-      <WelcomePage exact path="/"/>
+      <Route exact path="/">
+        <WelcomePage />
+      </Route>
       
-      <Route path='/characters/'>
+      <Route exact path='/characters/'>
         <Header />
         <SearchForm />
-      </Route>
-
-      <Route exact path='/characters/'>
         <CharacterList />
       </Route>
-
-      <Route path='/characters/:characterID'>
-        <CharacterCard />
-      </Route>
-
     </div>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 export default function SearchForm() {
  
   return (
     <section className="search-form">
-     // Add a search form here
+      <Form>
+        <FormGroup>
+          <Label for="search">Search</Label>
+          <Input type="search" name="search" id="search" placeholder="type charactername" />
+          <Button>Search</Button>
+        </FormGroup>
+      </Form>
     </section>
   );
 }
