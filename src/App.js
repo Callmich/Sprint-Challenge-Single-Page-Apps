5f8423a43e8df72eca4c42727297a66164c6447f
Michael Callahan <CallahanMichaelE@gmail.com>
2020-02-28 09:28:49 -0700

    inital skeleton built for app page using Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from "react";
+import { Route } from 'react-router-dom';
+
+import WelcomePage from './components/WelcomePage'
+import SearchForm from './components/SearchForm'
+import CharacterCard from './components/CharacterCard'
+import CharacterList from './components/CharacterList'
 import Header from "./components/Header.js";
 
 
 export default function App() {
   return (
-    <main>
+    <div>
+      <WelcomePage exact path='/'/>
+      
+      <Route path= '/characters/'>
       <Header />
-    </main>
+      <SearchForm />
+      <CharacterList />
+      </Route>
+
+      <Route exact path='/characters'>
+        <CharacterList />
+      </Route>
+
+      <Route path='/characters/:characterID'>
+        <CharacterCard />
+      </Route>
+
+    </div>
   );
 }
